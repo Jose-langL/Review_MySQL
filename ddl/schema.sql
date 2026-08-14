34e65b2b9c52fa122ced97b4ff15fecc9bae8cd1
Jose-langL <totherrerajoseluis@gmail.com>
2026-08-14 08:10:42 -0600

    feat: :sparkles: (ddl) New tables for Database

---
+++ ddl/schema.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE Review_BD;
+USE Review_BD;
+CREATE TABLE ciudad (
+    Ciudad_id VARCHAR(10) PRIMARY KEY,
+    Ciudad_Sede VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE sede (
+    Id VARCHAR(10) PRIMARY KEY,
+    nombre_sede VARCHAR(100) NOT NULL,
+    Ciudad_id VARCHAR(10) NOT NULL,
+    FOREIGN KEY (Ciudad_id) REFERENCES ciudad(Ciudad_id)
+);
+
+CREATE TABLE Socios (
+    Socio_ID INT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    Telefono VARCHAR(20)
+);
+
+CREATE TABLE planes_entrenamiento (
+    Id VARCHAR(10) PRIMARY KEY,
+    n_Entrenamiento VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE especialidades_entrenadores (
+    Id VARCHAR(10) PRIMARY KEY,
+    especialidad_Entrenador VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE entrenadores (
+    Id VARCHAR(10) PRIMARY KEY,
+    entrenadores_Asignados VARCHAR(100) NOT NULL,
+    especialidad_id VARCHAR(10) NOT NULL,
+    FOREIGN KEY (especialidad_id)
+        REFERENCES especialidades_entrenadores(Id)
+);
+
+CREATE TABLE Socio_Plan_Entrenamiento (
+    Id INT PRIMARY KEY,
+    Socio_ID INT NOT NULL,
+    planes_entrenamiento VARCHAR(10) NOT NULL,
+    Id_entrenador VARCHAR(10) NOT NULL,
+    Sede_id VARCHAR(10) NOT NULL,
+
+    FOREIGN KEY (Socio_ID)
+        REFERENCES Socios(Socio_ID),
+
+    FOREIGN KEY (planes_entrenamiento)
+        REFERENCES planes_entrenamiento(Id),
+
+    FOREIGN KEY (Id_entrenador)
+        REFERENCES entrenadores(Id),
+
+    FOREIGN KEY (Sede_id)
+        REFERENCES sede(Id)
+);
